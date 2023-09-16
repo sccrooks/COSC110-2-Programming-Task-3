24cff08b59aa53faee2ee0dff86bcbba5ee7603e
Scott Crooks <24785923+sccrooks@users.noreply.github.com>
2023-09-17 00:02:58 +1000

    Update clean.py

--- clean.py
+++ clean.py
@@ -12,23 +12,30 @@ class AverageRating(tk.Frame):
     def __init__(self, parent, *args, **kwargs):
         tk.Frame.__init__(self, parent, *args, **kwargs)
         self.parent = parent
+        self.cleanliness_average_str = "This bus has an average cleanliness rating of {}"
 
 
 class Application(tk.Frame):
     def __init__(self, parent, *args, **kwargs):
         tk.Frame.__init__(self, parent, *args, **kwargs)
         self.parent = parent
-        self.ratings_ui = RatingUI(self, ...)
 
+        self.greeting = tk.Label(text="Rate the cleanliness of this bus:")
+        self.ratings_ui = RatingUI(self)
+        self.average_rating = AverageRating(self)
+
+        self.greeting.pack()
         self.ratings_ui.pack(fill="x")
+        self.average_rating.pack()
 
+
+cleanliness_average = 0
 
 if __name__ == "__main__":
     root = tk.Tk()
     Application(root).pack(expand=True)
     root.mainloop()
 
-cleanliness_average = 0
 
-main_title_str = "Rate the cleanliness of this bus:"
-cleanliness_average_str = "This bus has an average cleanliness rating of {}"
+
+
