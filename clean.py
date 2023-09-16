6b7cb242ab8637aecdf5b1b424b2bea2dc343cd4
Scott Crooks <24785923+sccrooks@users.noreply.github.com>
2023-09-16 23:25:22 +1000

    Update clean.py

--- clean.py
+++ clean.py
@@ -2,4 +2,10 @@ import tkinter as tk
 
 window = tk.Tk()
 
+main_title_str = "Rate the cleanliness of this bus:"
+main_title = tk.Label(text=main_title_str)
+main_title.pack()
+
+cleanliness_average = "This bus has an average cleanliness rating of {}"
+
 window.mainloop()
