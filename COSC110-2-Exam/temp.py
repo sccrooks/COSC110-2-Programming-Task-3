6bc99784586e3b852c289936de313f8beb5c70ee
Scott Crooks <24785923+sccrooks@users.noreply.github.com>
2023-09-28 20:27:26 +1000

    Update temp.py

--- COSC110-2-Exam/temp.py
+++ COSC110-2-Exam/temp.py
@@ -1,16 +1,17 @@
-def mystery(num1, num2):
-  print("n1: " + str(num1))
+def mystery(num1, num2, depth):
+  print("\nn1: " + str(num1))
   print("n2: " + str(num2))
+  print("d: " + str(depth))
 
   if num1 == num2:
     return num1
   if num1 > num2:
-    return mystery(num2, num1)
+    return mystery(num2, num1, depth+1)
   if num2 % 5 == num1 % 5:
-    return mystery(num1, num2//2)
+    return mystery(num1, num2//2, depth+1)
 
-  
-  return mystery(num1, num2 + 1)
 
-value = mystery(8, 5)
+  return mystery(num1, num2 + 1, depth+1)
+
+value = mystery(8, 5, 0)
 print(value)
